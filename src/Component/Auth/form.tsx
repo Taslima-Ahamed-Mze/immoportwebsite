0d893cbc3d21903727fb09b0f6c5b8e7fa9aa671
bakhtaouiines <inesbkht@gmail.com>
2022-04-04 17:04:15 +0200

    retrieve input's value

--- src/Component/Auth/form.tsx
+++ src/Component/Auth/form.tsx
@@ -1,27 +1,38 @@
-import React from "react"
+import React, { useState } from 'react'
 
 // import { FC } from 'react' // we ensure that we return valid JSX 
 const AuthForm = () => {
-const handleSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
-    e.preventDefault()
-    console.log("J'ai cliqué")
-}
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log()
+    }
+
+    // const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     console.log(e.target.value);
+    // };
+
+    const [value, setValue] = useState('');
+    const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
+
     return (
         <div className="wrapper">
             <h1>Login</h1>
-            <form onSubmit={ handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <fieldset>
                     <label>
                         <p>Email</p>
-                        <input name="email" />
+                        <input name="email" value={value} onChange={handleInputValue} />
                     </label>
                     <label>
                         <p>Password</p>
-                        <input name="password" />
+                        <input name="password" type="password" />
                     </label>
                 </fieldset>
-                <button type="submit" >Submit</button>
+                <button type="submit">Submit</button>
             </form>
+            <div>Input value: {value}</div>
         </div>
     )
 }
