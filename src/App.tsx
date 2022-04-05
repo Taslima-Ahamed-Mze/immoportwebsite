49a0ba670144c7b6341911274ffd845a2f0d8b95
bakhtaouiines <inesbkht@gmail.com>
2022-04-05 12:26:53 +0200

    navbar render, work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AuthForm from './Component/Auth/form';
+import ReactDOM from 'react-dom';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Navbar from './Component/NavBar/Navbar';
+import AuthForm from './Component/NavBar/Auth/AuthForm';
 
+// ReactDOM.render(
+//   <StyledEngineProvider injectFirst>
+//     <Navbar />
+//   </StyledEngineProvider>,
+//   document.querySelector("#root")
+// );
 function App() {
   return (
     <div className="App">
