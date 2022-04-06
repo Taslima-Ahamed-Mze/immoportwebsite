a8e38c97ca512245d22724bc531d1384f4ab5ba5
bakhtaouiines <inesbkht@gmail.com>
2022-04-06 16:39:10 +0200

    mui CARD + modal work in progress

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from 'react';
 import { getProfile } from '../Api/Auth';
 import Client from '../Interface/Client';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
 
 const Profile = () => {
     const [token, setToken] = useState<string | null>(null)
@@ -9,7 +17,6 @@ const Profile = () => {
     useEffect(() => {
 
         if (token != null) {
-            console.log("token : " + token)
             getProfile(token)
                 .then(response => {
                     getDataProfile(response)
@@ -21,7 +28,43 @@ const Profile = () => {
 
     return (
         <div className="profileWrapper">
-            {dataProfile && <p>{dataProfile.lastname}</p>}
+            <Card sx={{ maxWidth: 275 }}>
+                <CardContent>
+
+                    {dataProfile &&
+                        <Typography sx={{ fontSize: 25 }} color="text.secondary" gutterBottom>
+                            {dataProfile.lastname}, {dataProfile.firstname}</Typography>}
+
+                    {dataProfile &&
+                        <Typography color="text.secondary">
+                            {dataProfile.mail}
+                        </Typography>}
+
+                    {dataProfile &&
+                        <Typography>
+                            {dataProfile.phone}
+                        </Typography>}
+
+                </CardContent>
+                <CardActions>
+                    <Button size="small"  onClick={handleOpen}>Modifier mon profil</Button>
+                </CardActions>
+            </Card>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                </Box>
+            </Modal>
         </div>
     )
 }
