392eefa45c386f081ac8c6ac42736b4e054a6784
bakhtaouiines <inesbkht@gmail.com>
2022-04-06 14:48:28 +0200

    fIX ISSUE on 401 status

--- src/Api/Auth.tsx
+++ src/Api/Auth.tsx
@@ -10,10 +10,9 @@ export const login = async (mail: string, password: string) => {
     }).then((response) => {
         if (response.data.access_token) {
             console.log(response.data);
-            localStorage.setItem("access_token", JSON.stringify(response.data.access_token));
+            localStorage.setItem("access_token", response.data.access_token);
             return response.data;
         }
-
     }).catch(error => {
 
         console.log(error.response.data.message);
