0d303ecdcb407fcdc3d5f8f019f386db97feb40c
bakhtaouiines <inesbkht@gmail.com>
2022-04-07 09:46:50 +0200

    clean import + rename navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import {
   Routes,
   Route,
-  Link
 } from 'react-router-dom';
 import './App.css';
-import ReactDOM from 'react-dom';
-import { StyledEngineProvider } from '@mui/material/styles';
 import Navbar from './Component/NavBar/Navbar';
 import AuthForm from './Component/NavBar/Auth/AuthForm';
-import { login } from './Api/Auth';
-import ResponsiveAppBar from "./Component/NavBar/Navbar";
 import RegisterForm from './Component/NavBar/Register/RegisterForm';
 import Profile from './Pages/Profile';
 
@@ -22,7 +16,7 @@ function App() {
 
       {/* Routes listing*/}
       <Routes>
-        <Route path="/" element={<ResponsiveAppBar />} />
+        <Route path="/" element={<Navbar />} />
         <Route path="/auth" element={<AuthForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/profile" element={<Profile />} />
